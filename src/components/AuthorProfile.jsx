b2ad1250841ce5ff386ff54ebbd50cd93900c2a7
Kush Agarwal <agarwalkushajay@gmail.com>
2024-04-16 23:38:00 +0530

    Changed Structure to apply styles

--- src/components/AuthorProfile.jsx
+++ src/components/AuthorProfile.jsx
@@ -8,15 +8,17 @@ import '../App.css';
 const AuthorProfile = () => {
   const { authorId } = useParams();
   const author = authorsData.find(author => author.id === parseInt(authorId));
+  console.log(author);
 
   if (!author) return <div className="container">Author not found</div>;
 
   return (
-    <div className="container">
-      <div className="blog">
+    <div className="container">      
+      <div className="blog profile">
+        <img src={"../" + author.image} alt="" className='user-img' />
         <h2>{author.name}</h2>
-        <p>{author.bio}</p>
         <p>{author.email}</p>
+        <p>{author.bio}</p>
       </div>
     </div>
   );
